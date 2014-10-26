9a32dcb119bb15e512271dbabd83d7bf25bb5380
William Fong <William@Williams-MacBook-Pro-2.local>
2014-10-25 18:45:49 -0600

    Added Frame object

--- src/com/wfong/token/STPLPFrame.java
+++ src/com/wfong/token/STPLPFrame.java
@@ -2,5 +2,8 @@ package com.wfong.token;
 
 public class STPLPFrame {
 	private int frameValue;
-	
+	public STPLPFrame (int frameValue) {
+		this.frameValue = frameValue;
+	}
+	//Acces Control Methods
 }
